dd1df40ec4feed20d4144aeb631caa38265ad5b7
sengathit <sengathit.l@gmail.com>
2019-06-22 22:11:48 -0500

    Custom adapter

--- app/src/main/java/sengathit/com/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/sengathit/com/coderswag/Controller/MainActivity.kt
@@ -2,12 +2,18 @@ package sengathit.com.coderswag.Controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import sengathit.com.coderswag.Adapters.CategoryAdapter
 import sengathit.com.coderswag.R
+import sengathit.com.coderswag.Services.DataService
 
 class MainActivity : AppCompatActivity() {
-
+    lateinit var adapter: CategoryAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        adapter = CategoryAdapter(this, DataService.categories)
+        categoryListView.adapter = adapter
     }
 }
